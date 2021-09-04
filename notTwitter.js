7e469ee30a53618ee2f4acf55dba3761e013c40d
Ferid Bathory <79676329+Void-Studios@users.noreply.github.com>
2021-09-04 15:04:56 -0500

    Add files via upload

---
+++ notTwitter.js
@@ -0,0 +1,33 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDSJcds4MfsJws0hUXJbF2cxV746JneZQw",
+  authDomain: "inazuma-pretty.firebaseapp.com",
+  databaseURL: "https://inazuma-pretty-default-rtdb.firebaseio.com",
+  projectId: "inazuma-pretty",
+  storageBucket: "inazuma-pretty.appspot.com",
+  messagingSenderId: "196428017978",
+  appId: "1:196428017978:web:986db6709393ef04b1e003",
+  measurementId: "G-1FWJ0NC05F"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
+  function adduser(){
+
+    unsername = document.getElementById("unsername_input").value;
+    console.log("unsername is: " + unsername)
+    
+    firebase.database().ref("/").child(unsername).update({
+    
+    maggothoney:"this is actually maggot honey",
+    notMaggotHoney:"this is not maggot honey",
+
+
+
+  });
+
+
+  window.location="notTwitter_room.html";
+
+}
