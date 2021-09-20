658864d25a3b626ee07a740ec5c902757ff2fb03
Ishita L <79676329+NPC-Bl00d1@users.noreply.github.com>
2021-09-20 18:19:54 -0500

    Add files via upload

--- notTwitter_room.js
+++ notTwitter_room.js
@@ -2,7 +2,7 @@
 
 
 var unsername = localStorage.getItem("unsername");
-document.getElementById("hello_there").innerHTML = "Welcome, " + unsername;
+
 
 
 
@@ -13,3 +13,21 @@ window.location="index.html";
 
 }
 
+function addRoom(){
+
+roomname = document.getElementById("roomname").value;
+localStorage.setItem("room_name",roomname);
+window.location="roompage.html";
+
+}
+
+
+function getData() {firebase.database().ref("/").on('value',
+function(snapshot) {document.getElementById("output").innerHTML =
+"";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+Room_names = childKey;
+//Start code
+
+//End code
+});});}
+getData();
