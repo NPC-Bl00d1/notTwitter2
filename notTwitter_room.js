2d594c5b2e944b96060606bd9a3a0acc066f1360
Ishita L <79676329+NPC-Bl00d1@users.noreply.github.com>
2021-10-11 18:54:31 -0500

    Add files via upload

--- notTwitter_room.js
+++ notTwitter_room.js
@@ -1,10 +1,22 @@
 
+var firebaseConfig = {
+    apiKey: "AIzaSyDSJcds4MfsJws0hUXJbF2cxV746JneZQw",
+    authDomain: "inazuma-pretty.firebaseapp.com",
+    databaseURL: "https://inazuma-pretty-default-rtdb.firebaseio.com",
+    projectId: "inazuma-pretty",
+    storageBucket: "inazuma-pretty.appspot.com",
+    messagingSenderId: "196428017978",
+    appId: "1:196428017978:web:986db6709393ef04b1e003",
+    measurementId: "G-1FWJ0NC05F"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 
 var unsername = localStorage.getItem("username");
 
 
-
+document.getElementById("hellothere").innerHTML=username;
 
 
 function logout(){
@@ -16,7 +28,7 @@ window.location="index.html";
 function addRoom(){
 
 roomname = document.getElementById("roomname").value;
-localStorage.setItem("room_name",roomname);
+localStorage.setItem("roomname",roomname);
 window.location="roompage.html";
 
 }
@@ -27,6 +39,8 @@ function(snapshot) {document.getElementById("output").innerHTML =
 "";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
 Room_names = childKey;
 //Start code
+
+
 
 //End code
 });});}
